afa8d8e92c1227e74c3b3e1105dc26841e8bb2c1
ManuelCes <ingmanuelcespedes@gmail.com>
2016-03-25 02:35:47 -0500

    Actualizando

---
+++ cdf.ControlMateriales_IngElectrica/src/java/Bean/Holamundo.java
@@ -0,0 +1,15 @@
+package Bean;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Manuel Cespedes
+ */
+public class Holamundo {
+    
+}
